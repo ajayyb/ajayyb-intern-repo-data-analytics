039bb37e7c8dff29cf1d5c4f7d9d80271be62e12
abel0027 <abel0027@student.monash.edu>
2025-03-17 11:12:58 +1100

    fix: milestone4 contained incorrect work

--- milestone4/messyCode.py
+++ milestone4/messyCode.py
@@ -1,4 +1,21 @@
-def test(word):
-    return print("testing", word)
+# Clear Code
+# Has clear function title
+# Less condenesed
+# Clear indentataion
+# Logic seperated into individual lines  
+def factorial(n):
+    if (n==1): 
+        return n
+    else:
+        return n * factorial(n-1)
 
-test("input")
+# Non-clear code
+# Unclear function name (not clear what function does)
+# Over condensed
+# No spaces between operands (or in areas which would make code clearer)
+def f(n):
+    if n<=1:return 1; 
+    else: return n*f(n-1)
+
+print(f(4))
+print(factorial(4))
